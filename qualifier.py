2b1433b950edfa159af9c19ad1a994bd092259ba
Ted V <58144503+BUSH222@users.noreply.github.com>
2023-08-17 02:31:32 +0300

    initial commit

---
+++ qualifier.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
+    """
+    Return True if the given input allows the rearrangement of the image, False otherwise.
+
+    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
+    once.
+    """
+    dimx = image_size[0]/tile_size[0]
+    dimy = image_size[1]/tile_size[1]
+    if dimx.is_integer() and dimy.is_integer():
+        if len(ordering) == dimx*dimy and set(ordering).issubset([0, 1, 2, 3]):
+            return True
+    return False
+
+def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
+    """
+    Rearrange the image.
+
+    The image is given in `image_path`. Split it into tiles of size `tile_size`, and rearrange them by `ordering`.
+    The new image needs to be saved under `out_path`.
+
+    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
+    once. If these conditions do not hold, raise a ValueError with the message:
+    "The tile size of ordering are not valid for the given image".
+    """
+
+    im = Image.open(image_path)
+    if not valid_input(image_size=im.size, tile_size=tile_size, ordering=ordering):
+        raise ValueError()
